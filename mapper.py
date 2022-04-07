3b950f28ce8e7527611c0da97c45d0f7b88a8651
PHLin <103209001+plin983@users.noreply.github.com>
2022-04-07 16:30:37 -0400

    Add files via upload

---
+++ mapper.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# this line means that the script is executable,
+# it calls the language interpreter to run the code inside the script
+# and is the guide to find 'python'
+
+
+# import the module for reading and writing data
+import sys
+
+#skip first line
+next(sys.stdin)
+# input is read by STDIN (standard input) and do the following for each
+# input line
+for line in sys.stdin:
+
+    # remove leading and trailing whitespace
+    line = line.strip()
+
+    # split the line by comma separator, a list is produced
+    line = line.split(",")
+
+    # assign first value of the list to the ticker
+    #ticker = line[0]
+    ticker = line[1]
+    
+    # assign first value of the list to the ticker
+    #ticker = line[0]
+    tradeDate = line[2]
+
+    # assign the third value to the trade price
+    #tradePrice = line[2]
+    # assign the 7th value to the volumn
+    tradeVolumn = line[7]
+    
+    # mapper prints ticker and trade price with a tab in between, which is
+    # taken as input by the reducer
+    if tradeDate[-4:] == '2018' :
+        print ('%s\t%s' % (ticker, tradeVolumn))
+
+
+
